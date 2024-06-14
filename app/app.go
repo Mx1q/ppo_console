895bc70c7b4f24deaaa51494fa43733d117f5a16
Mx1q <maxim.hal1tov@yandex.ru>
2024-06-14 13:19:52 +0300

    Adding connection.

--- app/app.go
+++ app/app.go
@@ -72,6 +72,22 @@ type Tui struct {
 	userInfo *services.JwtPayload
 }
 
+func NewConn(ctx context.Context, cfg *config.Config) (*pgxpool.Pool, error) {
+	connStr := fmt.Sprintf("%s://%s:%s@%s:%d/%s", cfg.Database.Driver, cfg.Database.User,
+		cfg.Database.Password, cfg.Database.Host, cfg.Database.Port, cfg.Database.DBName)
+
+	pool, err := pgxpool.New(ctx, connStr)
+	if err != nil {
+		return nil, fmt.Errorf("connecting to database: %w", err)
+	}
+
+	err = pool.Ping(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("pinging database: %w", err)
+	}
+	return pool, nil
+}
+
 func Run(db *pgxpool.Pool, cfg *config.Config, logger logger.ILogger) *tview.Application {
 	var tui Tui
 	tui.app = NewApp(db, cfg, logger)
